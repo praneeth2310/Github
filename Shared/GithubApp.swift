33a1a1725f3bb641d37c493fac60012c7a4fd976
praneeth2310 <praneeth.sakhamuri@gmail.com>
2021-09-28 23:41:12 -0400

    Initial Commit

---
+++ Shared/GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApp.swift
+//  Shared
+//
+//  Created by Praneeth Sakhamuri on 2021-09-28.
+//
+
+import SwiftUI
+
+@main
+struct GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
